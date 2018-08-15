62a7fa0931b5edc784f3eedda03f3396edce4d5b
Leonardo Ribeiro <leoribeiro.dsgn@gmail.com>
2018-08-14 23:29:38 -0300

    added first_name and last_name for user and created first tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should have the expected attributes'
+  it 'should have unique email'
 end
